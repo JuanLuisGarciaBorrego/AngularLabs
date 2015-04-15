9219b93cfbeb4afad58b415b9e343e050be6c5b7
Juan Luis <juanluisgarciaborrego@gmail.com>
2015-04-15 19:17:48 +0200

    ng-disable

---
+++ 5/app.js
@@ -0,0 +1,29 @@
+var app = angular.module("app",[]);
+
+
+function seguroController($scope) {
+    $scope.seguro = {
+        nif:"",
+        nombre:"",
+        ape1:"",
+        edad: undefined,
+        sexo: "",
+        casado: false,
+        coverturas: {
+            oftalmologia: false,
+            dental: false,
+            fecundacionInVitro:false
+        },
+        enfermedades:{
+            corazon:false,
+            estomacal: false,
+            rinyones: false,
+            alergia: false,
+            nombrealergia: ""
+        },
+        fechaCreacion:new Date()
+    }
+    $scope.disableNombreAlergia = function(){
+        return ($scope.seguro.enfermedades.alergia===false)
+    }
+}
