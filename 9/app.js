47b9ab341af314832ed5c27ad19e22e80a323fcf
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2015-04-22 14:37:01 +0200

    service http witch response=datos.json

---
+++ 9/app.js
@@ -0,0 +1,49 @@
+var app = angular.module("app",[]);
+
+app.controller("seguroController", function($scope, $log, $http){
+    $scope.seguro = {
+        nif:"",
+        nombre:"",
+        ape1:"",
+        edad: undefined,
+        sexo: "",
+        casado: false,
+        coverturas: {
+            oftalmologia: false,
+            dental: false,
+            fecundacionInVitro:false
+        },
+        enfermedades:{
+            corazon:false,
+            estomacal: false,
+            rinyones: false,
+            alergia: false,
+            nombrealergia: ""
+        },
+        fechaCreacion:new Date()
+    };
+
+    //Servicio Log
+    $log.debug("Acabamos de crear el $scope");
+
+    $scope.disableNombreAlergia = function(){
+        return ($scope.seguro.enfermedades.alergia===false)
+    };
+
+    $scope.soloMujeres = function(){
+        return ($scope.seguro.sexo=='M')
+    };
+
+    //http
+    var config={
+        method:"GET",
+        url: "datos.json"
+    };
+
+    var response = $http(config);
+
+    response.success(function(data,status, headers, config){
+        $scope.seguro = data;
+    });
+
+});
