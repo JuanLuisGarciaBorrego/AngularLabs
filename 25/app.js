39aeab1f0b410adda5d7614445ebd1cd5ac2da0f
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2015-05-04 11:53:17 +0200

    ng-class

---
+++ 25/app.js
@@ -0,0 +1,10 @@
+
+var app = angular.module("app",[]);
+
+app.controller("defaultController", function($scope){
+    $scope.clasesCss={
+        rojo:true,
+        subrayado:true,
+        negrita:false
+    }
+});
